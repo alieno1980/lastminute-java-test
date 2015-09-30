2a2744a691e77682c94f944c2eabf4c43f9d35ee
Giulio <gl.grosso@gmail.com>
2015-09-30 15:25:11 +0200

    Changed the factory manager class name

---
+++ src/main/java/com/lastminute/javatest/salestaxes/factory/FactoryManager.java
@@ -0,0 +1,17 @@
+package com.lastminute.javatest.salestaxes.factory;
+
+import com.lastminute.javatest.salestaxes.model.Item;
+
+public class FactoryManager {
+	
+	ItemFactory itemCategory;
+	
+	public void selectCategory(ItemFactory f) {
+		itemCategory = f;
+	}
+	
+	public Item createItem(String name, float price, boolean isImported) {
+		return itemCategory.createItem(name, price, isImported);
+	}
+	
+}
